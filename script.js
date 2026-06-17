c39a28a141f0dfe1ed441e6fa6e089b3602bf5cd
MAITRI2007 <maitrisharma916@gmail.com>
2026-06-17 23:03:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,61 @@ const observer = new IntersectionObserver(entries => {
 });
 
 sections.forEach(sec => observer.observe(sec));
+
+const projects = [
+  {
+    title: "Pre-Delinquency Engine",
+    image: "ChatGPT Image May 30, 2026, 10_17_42 PM.png",
+    problem: "Predictive system identifying loan default risk.",
+    tech: "HTML CSS JS",
+    demo: "TECH SQUAD(pre delinquency) - Made with RecordCast.mp4",
+    github: "#"
+  },
+
+  {
+    title: "Real Time Weather",
+    image: "Screenshot (639).png",
+    problem: "A weather dashboard that displays real-time weather data using API integration.",
+    tech: "React CSS API",
+    demo: "#",
+    github: "#"
+  }
+];
+
+let current = 0;
+
+function showProject(index) {
+
+  document.getElementById("projectImg").src =
+    projects[index].image;
+
+  document.getElementById("title").textContent =
+    projects[index].title;
+
+  document.getElementById("problem").textContent =
+    projects[index].problem;
+
+  document.getElementById("tech").textContent =
+    projects[index].tech;
+
+  document.getElementById("demo").href =
+    projects[index].demo;
+
+  document.getElementById("github").href =
+    projects[index].github;
+
+  document.getElementById("counter").textContent =
+    `${index + 1} / ${projects.length}`;
+}
+
+function nextSlide() {
+  current = (current + 1) % projects.length;
+  showProject(current);
+}
+
+function prevSlide() {
+  current = (current - 1 + projects.length) % projects.length;
+  showProject(current);
+}
+
+showProject(0);
